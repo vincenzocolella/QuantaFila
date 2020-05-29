d59c592ea95e22fd26f62b25d31b7df7bc751582
vincenzo <vincenzo.colella@hotmail.com>
2020-05-29 13:14:39 +0200

    v 1.3
    prima versione con db funzionante

---
+++ dati_elenco.php
@@ -0,0 +1,57 @@
+<?php
+
+
+    $errors = array('db' => false); //To store errors
+    $form_data = array(); //Pass back the data
+    $data = array();
+    
+    /* Validate the form on the server side */
+            
+        require 'db.php';
+        
+        $citta = $_POST['citta'];     
+
+        //questa va poi modificata con il db appartenente
+        $sql = "SELECT nome, tipo, indirizzo, attesa FROM attivitacommerciale WHERE Citta= ?;";
+        
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            $errors['db'] = true; //problma db
+            $form_data['posted'] = 'DB problem !';
+        }
+        else {
+            mysqli_stmt_bind_param($stmt, "s", $citta);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            while($riga = mysqli_fetch_assoc($result))
+                    {
+                    
+                        #echo $riga['nome'];
+                        #echo $riga['indirizzo']; // etc..
+                        $row = array();
+                        array_push($row, $riga['nome']);
+                        array_push($row, $riga['tipo']);
+                        array_push($row, $riga['indirizzo']);
+                        array_push($row, $riga['attesa']);
+                        array_push($data, $row);
+                    }  
+                    $form_data['success'] = true;
+                    $form_data['posted'] = 'Successo';
+                    $form_data['data'] = $data;
+                
+            }
+            
+        
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
+
+        if ($errors['db']) { //If errors in validation
+            $form_data['success'] = false;
+            $form_data['errors']  = $errors;
+        }
+
+        echo json_encode($form_data);
+
+
+
+?>
---
+++ homepage.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    /*var rows = document.getElementById("tabella").rows;
+    for (i = 0; i < rows.length; i++) {
+        rows[i].onclick = function(){ return function(){
+               var id = this.cells[0].innerHTML; //id = nome dell'attività
+               var indirizzo = this.cells[2].innerHTML;
+               alert("id:" + id);
+               alert("indirizzo: "+ indirizzo) */
+               //var IDUtenteNonAutenticato = '1';
+               var citta = 'Roma';
+               var ajaxRequest =$.ajax({
+                    type:'POST',
+                    url:'dati_elenco.php',
+                    dataType:'json',
+                    data:{citta:citta} });
+
+               
+                ajaxRequest.done(function(return_data){
+                if(return_data.success){
+
+                    //codice
+                    var trHTML = '';
+                    trHTML = "<table class='table' id='tabella'>"
+                                    + "<tr>" 
+                                        + "<th scope='col'>Nome</th>"
+                                        + "<th scope='col'>tipo</th>"
+                                        + "<th scope='col'>indirizzo</th>"
+                                        + "<th scope='col'>Attesa</th>"
+                                    + "</tr>";
+                        $.each(return_data.data, function (i,item) {
+                            trHTML += '<tr><td>' + item[0]
+                                + '</td><td>' + item[1]
+                                + '</td><td>' + item[2]
+                                + '</td><td>' + item[3]
+                                + '</td></tr>'; 
+                        });
+                        trHTML += "</table>";
+
+                        $('#tabella').append(trHTML);
+                    
+                    
+                }
+                else{
+                    alert(return_data.posted);
+                    }
+            });
+            ajaxRequest.fail(function(return_data){
+                alert("Errore con il server, riprovare!");
+            });
+            
+
+      /*  };}(rows[i]);
+    
+
+window.onload = addRowHandlers();  */
+});
